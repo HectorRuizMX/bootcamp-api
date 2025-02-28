71168c2f5ff8a1aa32655acd2a88e2a5b9d16ffa
HectorRuizMX <hector.ruiz@upnify.com>
2025-02-28 15:58:41 -0500

    Cambios a auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,14 @@ const auth = (req, res, next) => {
   const notAuthRoutes = ['/', '/login', '/register'];
   if (notAuthRoutes.includes(req.path)) return next();
 
-  const { authorization } = req.headers;
+  const Authorization = req.headers.authorization || req.headers.Authorization;
 
-  if (!authorization) {
+  if (!Authorization) {
     return res.status(403).send({ msg: 'Acceso denegado.'});
   }
 
   try {
-    const [, token] = authorization.split(' ');
+    const [, token] = Authorization.split(' ');
     jwt.verify(token, 'SECRET');
   } catch (error) {
     return res.status(403).send({ msg: "Acceso denegado." });
