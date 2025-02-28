13f5a4393d325263018ac350fffad86bca286df0
HectorRuizMX <hector.ruiz@upnify.com>
2025-02-28 17:58:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, cache-control, token, X-Forwarded-Port, X-Forwarded-Proto, X-Forwarded-For');
   res.header('Access-Control-Expose-Headers', 'Row-Count, X-RateLimit-Reset');
+  next();
 });
 app.use(auth);
 app.use(bodyParser.json());
